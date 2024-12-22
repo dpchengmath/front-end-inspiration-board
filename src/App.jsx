1adaaf835406c55f71e4bae0167fd1aeb915b22d
shiyinxi <shiyinxi@gmail.com>
2024-12-21 23:18:35 -0800

    more css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css';
 import BoardList from './components/BoardList';
+import NewBoardForm from './components/NewBoardForm';
 
 const App = () => {
   const onClickCallback = (id) => {
@@ -37,27 +38,11 @@ const App = () => {
         </section>
         <section>
           <h2>Create a New Board</h2>
-          {/* <NewBoardForm /> */}
+          <NewBoardForm />
         </section>
       </section>
     </div>
-        <section className="boards__container">
-          <section>
-            <h2 className="logo">Boards</h2>
-            <ol className="boards__list">
-              <li><div>Pick-me-up Quotes</div></li>
-              <li><div>sailor moon</div></li>
-            </ol>
-          </section>
-          <section>
-            <h2>Selected Board</h2>
-            <p>Select a Board from the Board List!</p>
-          </section>
-          <section>
-            <h2>Create a New Board</h2>
-            {/* <NewBoardForm /> */}
-          </section>
-        </section>
+       
       </div>
   );
 };
--- src/components/NewBoardForm.jsx
+++ src/components/NewBoardForm.jsx
@@ -2,29 +2,36 @@ import {useState} from 'react';
 import PropTypes from 'prop-types';
 import './NewBoardForm.css';
 
-const NewBoardForm = ({addBoardCallback}) => {
-
+const NewBoardForm = () => {
 
   return (
     <section >
-      <h2>Create a New Board</h2>
-      <form className="new-board-form__form" onSubmit={onSubmit}>
-        <label>Title</label>
-        <input
-          type="text"
-          value={title}
-          onChange={onTitleChange}
-        />
-        <label>Owner's Name</label>
-        <input
-          type="text"
-          value={owner}
-          onChange={onOwnerChange}
-        />
-        <p>Preview: {title} - {owner}</p>
+      <form className="new-board-form__form" >
+        <div>
+          <label>Title</label> 
+        </div>
+        <div>
+          <input
+            type="text"
+            // value={title}
+            // onChange={onTitleChange}
+          />
+        </div>
+        <div>
+          <label>Owner's Name</label>  
+        </div>
+        <div>
+          <input
+            type="text"
+            // value={owner}
+            // onChange={onOwnerChange}
+          />
+        </div>
+
+        <p>Preview:    </p>
         <input
           type="submit"
-          disabled={!title || !owner}
+          // disabled={!title || !owner}
         />
       </form>
       <span class="new-board-form__toggle-btn">Hide New Board Form</span>
