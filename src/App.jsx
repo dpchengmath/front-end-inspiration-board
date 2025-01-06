92c9f6f42a5043d18e515a650ff9361f86a73344
shiyinxi <shiyinxi@gmail.com>
2025-01-05 21:57:01 -0800

    add get cards api call

--- src/App.jsx
+++ src/App.jsx
@@ -29,63 +29,68 @@ const convertFromBoardApi = (board) => {
   return newBoard;
 };
 
-// const convertFromCardApi = (card) => {
-//   const newCard = {
-//     ...card,
-//     id: card.card_id,
-//     likesCount: card.likes_count,
-//     boardId: card.board_id,
-//   };
-//   delete newCard.card_id;
-//   delete newCard.likes_count;
-//   delete newCard.board_id;
-//   return newCard;
-// };
+const getAllCardsApi = (boardId) => {
+  return axios.get(`${kbaseURL}/boards/${boardId}/cards`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error('Error fetching cards', error);
+    });
+};
 
-// const getAllCardsApi = (boardId) => {
-//   return axios.get(`${kbaseURL}/boards/${boardId}/cards`)
-//     .then((response) => {
-//       return response.data;
-//     })
-//     .catch((error) => {
-//       console.error('Error fetching cards', error);
-//     });
-// };
+const convertFromCardApi = (card) => {
+  const newCard = {
+    ...card,
+    id: card.card_id,
+    likesCount: card.likes_count,
+    boardId: card.board_id,
+  };
+  delete newCard.card_id;
+  delete newCard.likes_count;
+  delete newCard.board_id;
+  return newCard;
+};
 
-// const deleteCardApi = (cardId) => {
-//   return axios.delete(`${kbaseURL}/cards/${cardId}`)
-//     .catch((error) => {
-//       console.error('Error deleting card', error);
-//     });
-// };
+const deleteCardApi = (cardId) => {
+  return axios.delete(`${kbaseURL}/cards/${cardId}`)
+    .catch((error) => {
+      console.error('Error deleting card', error);
+    });
+};
 
 const App = () => {
-
-  // const [boardsData, setBoardsData] = useState([{
-  //   id: 1,
-  //   title: 'Pick-me-up Quotes',
-  //   owner: 'Sunitha',
-  // },
-  // {
-  //   id: 2,
-  //   title: 'Test board',
-  //   owner: 'Lorraine',
-  // }]);
 
   const [boardsData, setBoardsData] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState(null);
   const [cardsData, setCardsData] = useState([]);
-
+  
   const getAllBoards =() => {
     getAllBoardsApi()
-      .then((data) => {
-        setBoardsData(data['boards'].map(convertFromBoardApi));
-      });
+    .then((data) => {
+      setBoardsData(data['boards'].map(convertFromBoardApi));
+    });
   };
   
+  const getCardsForSelectedBoard = () => {
+    getAllCardsApi(selectedBoard.id)
+      .then((data) => {
+        setCardsData(data['cards'].map(convertFromCardApi));
+      });
+  };
+
   useEffect(() => { 
     getAllBoards(); 
-  }, []);
+  }, [boardsData]);
+
+  useEffect(() => {
+    if (selectedBoard?.id) {
+      getCardsForSelectedBoard();
+    } else {
+      setCardsData([]); 
+    }
+  }, [selectedBoard]);
+  
 
   const onBoardClick = (id) => {
     const clickedBoard = boardsData.find((board) => board.id === id);
@@ -95,31 +100,30 @@ const App = () => {
 //   const [isBoardFormVisible, setIsBoardFormVisible] = useState(true);
 
 
-//   const handleBoardSubmit = (newBoard) => {
-//     return axios.post(`${kbaseURL}/boards`, newBoard)
-//       .then((response) => {
-//         setBoardsData([...boardsData, convertFromBoardApi(response.data)]);
-//       })
-//       .catch((error) => {
-//         console.error('Error creating board', error);
-//       });
-//   };
-
-
-const addBoard = (newBoard) => {
-  const newBoardWithId = { ...newBoard, id: boardsData.length + 1 };
-  setBoardsData([...boardsData, newBoardWithId]);
-};
-
-const addCard = (newCard) => {
-  if (selectedBoard) {
-    const newCardWithId = { ...newCard, id: cardsData.length + 1, boardId: selectedBoard.id };
-    setCardsData([...cardsData, newCardWithId]);
-  }
-};
-  const getCardsForSelectedBoard = () => {
-    return cardsData.filter(card => card.boardId === selectedBoard?.id);
+  const handleBoardSubmit = (newBoard) => {
+    return axios.post(`${kbaseURL}/boards`, newBoard)
+      .then((response) => {
+        setBoardsData((prevBoards)=>[...prevBoards, convertFromBoardApi(response.data)]);
+      })
+      .catch((error) => {
+        console.error('Error creating board', error);
+      });
   };
+
+
+// const addBoard = (newBoard) => {
+//   const newBoardWithId = { ...newBoard, id: boardsData.length + 1 };
+//   setBoardsData([...boardsData, newBoardWithId]);
+// };
+
+  const addCard = (newCard) => {
+    if (selectedBoard) {
+      const newCardWithId = { ...newCard, id: cardsData.length + 1, boardId: selectedBoard.id };
+      setCardsData([...cardsData, newCardWithId]);
+    }
+  };
+
+
   const handleLikeCardClick = (id) => {
     // Handle like card click if needed
   };
@@ -146,14 +150,14 @@ const addCard = (newCard) => {
         </section>
         <section className="new-board-form__container">
           <h2>Create a New Board</h2>
-          <NewBoardForm addBoardCallback={addBoard} />
+          <NewBoardForm addBoardCallback={handleBoardSubmit} />
         </section>
       </section>
       {selectedBoard && (
         <section className="cards__container">
           <section>
             <h2>Cards For {selectedBoard.title}</h2>
-            <CardList cards={getCardsForSelectedBoard()}
+            <CardList cards={cardsData}
             onLikeCardClick={handleLikeCardClick}
             onDeleteCard={handleDeleteCard} />
           </section>
